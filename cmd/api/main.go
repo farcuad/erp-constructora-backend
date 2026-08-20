5c4e1e824ae60fadf85b4a6a127d75c2b38b7dd7
Andres <andrescalles2012@gmail.com>
2026-08-19 22:34:50 -0400

    Agregado metodo PATCH en cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -28,7 +28,7 @@ func enableCORS(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-API-Key")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
